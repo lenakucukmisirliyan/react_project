a8b4300ed26d64f36d653e59ac13f4867a874c4c
Lena Küçükmısırlıyan <tlenakucukmisirliyan@epias.com.tr>
2025-06-18 11:55:23 +0300

    forth commit

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,21 +1,26 @@
-function Movies({lang}) {
-    return (
+import React, { useEffect, useState } from "react";
+
+const API_KEY = "23a0dfb4e623e111fa20f927a8922a98" 
+
+function MovieList () {
+    const [movies, setMovies] = useState([]);
+
+    useEffect (() => {
+        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=tr-TR&page=1`)
+            .then(res => res.json())
+            .then(data => setMovies(data.results))
+            .catch(err => console.error("Veri alınamadı" ,err));
+    }, []);
+
+    return(
         <div>
-            <h1>{lang === 'en' ? 'Favorite Movies' : 'Sevdiğim Filmler'}</h1>
-                <ul>
-                    <li>{lang == 'en' ? 'Lorem, ipsum dolor' : 'Totam, aliquam iure?'}</li>
-                    <li>{lang == 'en' ? 'Voluptas, ipsa recusandae' : 'Officia, cum autem?'}</li>
-                    <li>{lang == 'en' ? 'Optio, soluta saepe.' : 'Tempora, illum aliquam.'}</li>
-                    <li>{lang == 'en' ? 'Vel, explicabo eos!' : 'Officia, cum autem?'}</li>
-                    <li>{lang == 'en' ? 'Mollitia, in voluptate!' : 'Officia, cum autem?'}</li>
-                    <li>{lang == 'en' ? 'empora, illum aliquam.' : 'Officia, cum autem?'}</li>
-                    <li>{lang == 'en' ? 'Delectus, animi doloremque.' : 'Officia, cum autem?'}</li>
-                    <li>{lang == 'en' ? 'Delectus, sequi dignissimos.' : 'Officia, cum autem?'}</li>
-                    <li>{lang == 'en' ? 'Totam, aliquam iure?' : 'Officia, cum autem?'}</li>
-                    <li>{lang == 'en' ? 'Officia, cum autem?' : 'Officia, cum autem?'}</li>
-                </ul>
+            <ul>
+                {movies.slice(0, 10).map(movie => (
+                    <li key={movie.id}>{movie.title}</li>
+                ))}
+            </ul>
         </div>
     );
 }
 
-export default Movies;
+export default MovieList;
